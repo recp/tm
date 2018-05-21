c63b33fc5967af3899cb86ee98e9ad1dd7df6d40
Recep Aslantas <info@recp.me>
2018-05-21 12:04:05 +0300

    fix sleep for windows

--- src/win/sleep.c
+++ src/win/sleep.c
@@ -3,12 +3,10 @@
  * MIT License (MIT), http://opensource.org/licenses/MIT
  */
 
-#define WIN32_LEAN_AND_MEAN
-
 #include "../common.h"
 #include "../../include/tm/sleep.h"
 
-#include <windows.h>
+#include <stdio.h>
 
 TM_EXPORT
 void
@@ -18,8 +16,9 @@ tm_sleep(double secs) {
 
   if (secs <= 0.0 || !(timer = CreateWaitableTimer(NULL, TRUE, NULL)))
     return;
-  
-  li.QuadPart = (LONGLONG)(-secs * 1e-9);
+
+  /* 100 nanosecond intervals */
+  li.QuadPart = (LONGLONG)(-secs * 1e7);
   if (!SetWaitableTimer(timer, &li, 0, NULL, NULL, FALSE)) {
     CloseHandle(timer);
     return;
