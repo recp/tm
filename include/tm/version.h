c97e230297a5c4269be6cd185252586f32da63f3
Recep Aslantas <info@recp.me>
2018-05-21 12:31:41 +0300

    add version infoadd verison header

---
+++ include/tm/version.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ *
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ * Full license can be found in the LICENSE file
+ */
+
+#ifndef tm_version_h
+#define tm_version_h
+
+#define TM_VERSION_MAJOR 0
+#define TM_VERSION_MINOR 1
+#define TM_VERSION_PATCH 0
+
+#endif /* tm_version_h */
